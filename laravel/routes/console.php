3713bf47e7341025fb639bd20c5fe0e02dfd58b8
letoceiling-coder <dsc-23@yandex.ru>
2026-02-11 22:49:26 +0300

    Explicitly register ImportJsonCommand in console routes

--- laravel/routes/console.php
+++ laravel/routes/console.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Console\Commands\ImportJsonCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Explicitly register the import command
+Artisan::registerCommand(new ImportJsonCommand());
